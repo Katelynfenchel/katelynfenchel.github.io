9692bd47c44c5563b2e71fde1473cf25cd279f5c
Katelyn Fenchel <katelyn.fenchel@gmail.com>
2017-02-17 12:15:08 -0600

    shufflefuntionworks

--- game_project/js/app.js
+++ game_project/js/app.js
@@ -12,18 +12,37 @@
 //function is it the first or second click
 //if it is the first click leave  then tunr the second click
 
-$(document).ready(function(){
-//function to allow to do several functions
-	var cards= [12,12,99,99,101,101,1,1,2,2,8,8]
-	var game={
-		init: function(){
+// $(document).ready(function(){
+//function to allow to do several functions and create a array of numbers for the cards
+	console.log("hi")
 
+	var game = {
+	 cards:[1,1,2,2,3,3,4,4,5,5,6,6],
+		init:function(){
+			shuffle();
+		},
+		//create a shuffle funtion for array of numbers
+		shuffle: function(){
+			var random=0;
+			var temp=0;
+			//create a for loop to go through the array of cards and pick randomly
+			for(i=0; i<this.cards.length; i++){
+				random=Math.floor(Math.random() * i);
+				console.log(random)
+				// temp var is equal to a array of numbers
+				temp=this.cards[i];
+				//temp will equal another array of numbers at random
+				this.cards[i]=this.cards[random];
+				//that random array will now equal the temp variable
+				this.cards[random]=temp;
+			}
+			console.log(game.cards)
 		},
 		//create a click function
-		click: function(){
+		clickHandlers: function(){
 
-		}
+		},
 	}
 
-game.init()
-});
+// game.init()
+// });
