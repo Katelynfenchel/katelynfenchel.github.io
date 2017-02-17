fa896df8a5f7a0c6f197ca01fdd52896797ad751
Katelyn Fenchel <katelyn.fenchel@gmail.com>
2017-02-17 09:25:17 -0600

    Project1

---
+++ game_project/js/app.js
@@ -0,0 +1,17 @@
+//show back of card 
+
+
+
+// $( 'img' ).click(function(){
+// 	console.log('its working')
+// 	//when you click the function make the card flip
+// 	$( this ).prop('src', 'images/backOfCard.png');
+	
+// });
+
+//function is it the first or second click
+//if it is the first click leave  then tunr the second click
+
+$(document).ready(function(){
+
+});
