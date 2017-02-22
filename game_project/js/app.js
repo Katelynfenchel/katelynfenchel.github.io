5476dc3bd08636709bf4a13ef47c3c4c5b3f701d
Katelyn Fenchel <katelyn.fenchel@gmail.com>
2017-02-22 11:03:04 -0600

    added more functions

--- game_project/js/app.js
+++ game_project/js/app.js
@@ -1,14 +1,29 @@
 
 
-var count = 0;
+
 $(document).ready(function(){
+
+// use jquery to select the button
+// add an event listener
+// inside of that call your function to start the game
+$('#resetGame').click(function(){
+		games.reset();
+
+})
+
+
+
+
 //function to allow to do several functions and create a array of numbers for the cards
 	var games = {
+		count: '',
 	 	cards:['images/helloKittyBatman.jpg','images/helloKittyBatman.jpg','images/helloKittyExercise.png','images/helloKittyExercise.png',
 	 	'images/helloKittyStrawberry.jpg','images/helloKittyStrawberry.jpg','images/helloKittyNinja.png','images/helloKittyNinja.png','images/helloKittyShop.jpg','images/helloKittyShop.jpg',
 	 	'images/helloKittyZebra.jpg','images/helloKittyZebra.jpg'],
 		init:function(){
-			games.shuffle();
+			this.shuffle();
+			this.assignCard();
+			this.clickHandlers();
 		},
 		//create a shuffle funtion for array of numbers
 		shuffle: function(){
@@ -23,9 +38,10 @@ $(document).ready(function(){
 				this.cards[i]=this.cards[random];
 				this.cards[random]=temp;
 			}
-			games.assignCard();
-			// console.log('shuffle card array:'+games.cards)
+			
+			console.log('shuffle card array:'+games.cards)
 		},
+
 		 assignCard:function(){
 		 	// console.log(this, ' in assignCard')
 
@@ -37,31 +53,37 @@ $(document).ready(function(){
 			 	// // create a data value and assign value(number) to the card 
 			 	// 	console.log(self.cards, ' this is self.cards')
 			 	// 	console.log(this.cards, ' this is this.cards')
+			 	// add the the selected and not matched back to all the cards
 
-		 		$(this).attr('data-card-value', games.cards[index]);
+			 	$(this).animate({opacity: 100}).addClass('notmatched');
+		 		$(this).attr('data-card-value', self.cards[index]);
+
+		 		// $(this).html(('<img src="' + $(this).data('cardValue') + '"></img>'))
+		 		//http://www.cartoon-clipart.co/amp/images/hello-kitty4.png
+		 		//$(this).data('cardValue')
 		 		 //$(this).attr( games.cards[index]);
-		 	}),
-		 	games.clickHandlers();
-		 },
-		 // var count = 0;
-		 clickHandlers: function(){
+		 	})
 		 
+		 },
+		 
+		 clickHandlers: function(){
+			 var self= this;
+			 //console.log(this, ' this is this')
 			$('.card').on('click', function(){
+				// console.log('click handler is happening')
 				console.log($(this).data('cardValue'));
-		 		// $(this).html('<p>' + $(this).data('cardValue')+ '</p>').addClass('selected');
-		 		//
+				console.log($(this).data('cardValue'));
 		 		$(this).html('<img src="' + $(this).data('cardValue') + '"></img>').addClass('selected');	
-		 		games.checkMatch();
-		 		count += 1;
-		 		$('#counter').text(count);
-				console.log(count);
+		 		self.checkMatch();
+		 		this.count += 1;
+		 		$('#counter').text(self.count);
+				// console.log(count);
 		 	});
-			// // count += 1;
-			// $('#counter').text(count);
-			// console.log(count);
 		 },
 		 checkMatch: function(){
+		 	//console.log('chekc math')
 		 	if($('.selected').length == 2){
+		 		console.log('check match is happen ing')
 		 		if ($('.selected').first().data('cardValue') == $('.selected').last().data('cardValue')){
 		 			$('.selected').each(function(){
 		 				$(this).animate({opacity: 0}).removeClass('notmatched');
@@ -71,8 +93,9 @@ $(document).ready(function(){
 
 			});
 		 			
-		 		games.checkWinner();
+		 		this.checkWinner();
 			} else{
+
 				setTimeout(function() {
 					$('.selected').each(function(){
 						$(this).html('').removeClass('selected');
@@ -86,13 +109,33 @@ $(document).ready(function(){
 	},
 	checkWinner: function(){
 		if($('.notmatched').length === 0){
-			$('.container').html('<h3> You are the winner!</h3>');
+			$('.container').html('<h3> You Won, Horray!! </h3>');
 		}
+	},
+	reset:function (){
+		this.removeImage();
+		this.shuffle();
+		this.assignCard();
+		this.clickHandlers();
+		this.upDateImages();
+		
+	},
+
+	removeImage: function(){
+		$('.card').each(function(index){
+			$(this).empty();
+		})
+	},
+
+	upDateImages: function(){
+		$('img').each(function(index){
+			//grab the parent data card value and set it to the source image
+		})
 	}
-};
+}
 	
-	games.init();
-	// var count = 0;
+games.init();
+	
 });
 
 
